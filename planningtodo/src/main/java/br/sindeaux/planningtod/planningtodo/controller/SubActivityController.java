24e44cb09a733e71fcccdbff1873106e191bb801
Marcio Alexandre Freire Sindeaux <sindeaux1999@hotmail.com>
2019-10-01 22:02:44 -0300

    feature: Criação de end-point de cadastro de subatividadea

--- planningtodo/src/main/java/br/sindeaux/planningtod/planningtodo/controller/SubActivityController.java
+++ planningtodo/src/main/java/br/sindeaux/planningtod/planningtodo/controller/SubActivityController.java
@@ -1,8 +1,10 @@
 package br.sindeaux.planningtod.planningtodo.controller;
 
 import br.sindeaux.planningtod.planningtodo.dto.base.SubActivityDTO;
+import br.sindeaux.planningtod.planningtodo.entity.SubActivitys;
 import br.sindeaux.planningtod.planningtodo.repository.SubActivityRepository;
 import br.sindeaux.planningtod.planningtodo.service.SubActivityService;
+import br.sindeaux.planningtod.planningtodo.utils.ConverterUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,13 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("sub-activity")
-public class SubActivityController {
+public class SubActivityController implements  AbstractController{
 
     @Autowired
     private SubActivityService subActivityService;
 
     @PostMapping
     public ResponseEntity cadastrarSubAtividade(@RequestBody SubActivityDTO subActivityDTO){
-        try{}catch (Exception e){}
+        try{
+            SubActivitys subActivity = ConverterUtils.convertTo(subActivityDTO, SubActivitys.class);
+            subActivityService.salvarNovaSubAtividade(subActivity);
+            return tryResponse();
+        }catch (Exception e){
+            return catchResponse();
+        }
     }
 }
--- planningtodo/src/main/java/br/sindeaux/planningtod/planningtodo/dto/base/SubActivityDTO.java
+++ planningtodo/src/main/java/br/sindeaux/planningtod/planningtodo/dto/base/SubActivityDTO.java
@@ -4,20 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SubActivityDTO {
+
     private Long id;
 
     private LocalDate finishDate;
 
-    private Boolean finish;
+    private boolean finish;
 
+    @NotNull @NotEmpty @NotBlank
     private String annotations;
 
+    @NotNull
     private ActivityDTO activity;
 
 }
